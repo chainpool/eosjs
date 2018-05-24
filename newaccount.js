1eb56bd44ed0c1ad29bc6f492ef21709f6e6553c
gguoss <guanghuaguo.oss@gmail.com>
2018-05-24 16:48:00 +0800

    add updatebp

--- newaccount.js
+++ newaccount.js
@@ -3,12 +3,13 @@ Eos = require('./src') // Eos = require('./src')
 wif = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
 pubkey = 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV'
 
-eos = Eos.Localnet({keyProvider: wif})
+eos = Eos.Localnet({keyProvider: '5KfoXJLsWZTRQ6gmLUgrnd5k5G5SucSfE4299eRfL5MmDTHKL5e',
+                    httpEndpoint: 'http://192.168.1.234:8888'})
 
 eos.transaction(tr => {
   tr.newaccount({
-    creator: 'eosio',
-    name: 'ntract11',
+    creator: 'twryy4swtrj3',
+    name: 'ggg',
     owner: pubkey,
     active: pubkey
   })
--- updatebp.js
+++ updatebp.js
@@ -1,14 +1,17 @@
 Eos = require('./src') // Eos = require('./src')
 
-eos = Eos.Localnet({keyProvider: '5KfoXJLsWZTRQ6gmLUgrnd5k5G5SucSfE4299eRfL5MmDTHKL5e',
+wif = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+eos = Eos.Localnet({keyProvider: wif,
                     httpEndpoint: 'http://192.168.1.234:8888'})
 
 // Run with no arguments to print usage.
-eos.transfer()
+eos.updatebp()
 
 // Usage with options (options are always optional)
 options = {broadcast: false}
 
+pubkey = 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV'
+
 eos.transaction(tr => {
-     tr.transfer({from: 'twryy4swtrj3', to: 'ggg', quantity: '1 EOS', memo: ''}, options)
+     tr.updatebp({bpname: 'ggg', producer_key:pubkey, commission_rate: 1}, options)
      })
